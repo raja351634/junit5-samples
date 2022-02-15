ff32dbba02318a1e987bf32c99f38ab36203c8d2
Rajaram <rajaram.accet@gmail.com>
2022-02-15 15:52:12 -0500

    Halfway Thru Junit

--- junit5-jupiter-starter-maven/src/main/java/com/example/project/Calculator.java
+++ junit5-jupiter-starter-maven/src/main/java/com/example/project/Calculator.java
@@ -16,4 +16,11 @@ public class Calculator {
 		return a + b;
 	}
 
+	public int multiply(int a, int b) {
+		return a * b;
+	}
+
+	public int divide (int a, int b) {
+		return a/b;
+	}
 }
--- junit5-jupiter-starter-maven/src/test/java/com/example/project/CalculatorTests.java
+++ junit5-jupiter-starter-maven/src/test/java/com/example/project/CalculatorTests.java
@@ -11,21 +11,32 @@
 package com.example.project;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.condition.EnabledIf;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.OS;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 class CalculatorTests {
 
+	private static Calculator calculator;
+
+	@BeforeAll
+	 static void intializeCalcualtor() {
+		calculator = new Calculator();
+	}
+
 	@Test
 	@DisplayName("1 + 1 = 2")
 	void addsTwoNumbers() {
-		Calculator calculator = new Calculator();
+	//	Calculator calculator = new Calculator();
 		assertEquals(2, calculator.add(1, 1), "1 + 1 should equal 2");
 	}
 
+	@EnabledOnOs(OS.WINDOWS)
 	@ParameterizedTest(name = "{0} + {1} = {2}")
 	@CsvSource({
 			"0,    1,   1",
@@ -34,8 +45,28 @@ class CalculatorTests {
 			"1,  100, 101"
 	})
 	void add(int first, int second, int expectedResult) {
-		Calculator calculator = new Calculator();
+	//	Calculator calculator = new Calculator();
 		assertEquals(expectedResult, calculator.add(first, second),
 				() -> first + " + " + second + " should equal " + expectedResult);
+	}
+
+	@Test
+	@DisplayName("Multiplicatio Basic - 2 * 2 = 4")
+	void multiplyTwoNumbers() {
+	//	Calculator calculator = new Calculator();
+		Assertions.assertAll("Test All",() -> Assertions.assertNotNull(calculator)
+		,()-> assertEquals(4,calculator.multiply(2,2)));
+	}
+
+	@Test
+	@DisplayName("Checking Divide by zero")
+	@EnabledIf("testDivideByZero")
+	void testException () {
+		Assumptions.assumeFalse("Raja".equals("Ram"));
+		assertThrows(ArithmeticException.class,() -> calculator.divide(1,0));
+	}
+
+	boolean testDivideByZero() {
+		return false;
 	}
 }
